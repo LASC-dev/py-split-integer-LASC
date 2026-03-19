3b0ee32759ca8a0d7882ede7cb01dc23c3ec7867
LASC-dev <lucas.a.s.casas@outlook.es>
2026-03-19 11:13:12 -0300

    fix: resolve syntax errors in asserts and fix package structure
    test: implement max/min difference validation

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,6 +2,30 @@ import pytest
 from app.split_integer import split_integer
 
 
+@pytest.mark.parametrize(
+    "value, num_parts", [
+        (8, 1),
+        (6, 2),
+        (17, 4),
+        (32, 6)
+    ]
+)
+def test_difference_between_max_and_min_should_be_less_than_or_equal_to_one(
+    value: int,
+    num_parts: int
+) -> None:
+    res = split_integer(value, num_parts)
+    assert max(res) - min(res) <= 1
+
+
+@pytest.mark.parametrize(
+    "value, num_parts", [
+        (8, 1),
+        (6, 2),
+        (17, 4),
+        (32, 6)
+    ]
+)
 def test_sum_of_the_parts_should_be_equal_to_value(
     value: int,
     num_parts: int
@@ -9,6 +33,14 @@ def test_sum_of_the_parts_should_be_equal_to_value(
     assert sum(split_integer(value, num_parts)) == value
 
 
+@pytest.mark.parametrize(
+    "value, num_parts", [
+        (7, 1),
+        (49, 7),
+        (100, 10),
+        (8, 4)
+    ]
+)
 def test_should_split_into_equal_parts_when_value_divisible_by_parts(
     value: int,
     num_parts: int
@@ -16,21 +48,42 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts(
     assert len(set(split_integer(value, num_parts))) == 1
 
 
+@pytest.mark.parametrize(
+    "value", [
+        7,
+        8,
+        137,
+        66
+    ]
+)
 def test_should_return_part_equals_to_value_when_split_into_one_part(
     value: int
 ) -> None:
-    split_once = split_integer(value, 1)
-    assert split_once[0] == value
+    assert split_integer(value, 1)[0] == value
 
 
+@pytest.mark.parametrize(
+    "value, num_parts", [
+        (13, 3),
+        (17, 4),
+        (100, 10)
+    ]
+)
 def test_parts_should_be_sorted_when_they_are_not_equal(
     value: int,
     num_parts: int
 ) -> None:
-    sort_split = sorted(split_integer(value, num_parts))
-    assert sort_split == split_integer(value, num_parts)
+    result = split_integer(value, num_parts)
+    assert sorted(result) == result
 
 
+@pytest.mark.parametrize(
+    "value, num_parts", [
+        (5, 10),
+        (2, 8),
+        (4, 3)
+    ]
+)
 def test_should_add_zeros_when_value_is_less_than_number_of_parts(
     value: int,
     num_parts: int
